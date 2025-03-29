fb03c14c3a82cf67e97fe9b74d0f5827f586f854
生川晴己 <oikawaharuonore@NaruMac.local>
2025-03-30 02:57:47 +0900

    javaを変更
    ヘッダーのトップをクリックするとTOPに戻ります

--- java/index.java
+++ java/index.java
@@ -1,3 +1,14 @@
+// ヘッダーのボタン
+const topButton = document.querySelector('#header a.nav-link top');
+// クリックした時の位置を取得
+topButton.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0, 
+        behavior: 'smooth' 
+    });
+});
+
+
 // フッターのボタン
 const topButton = document.querySelector('#footer div.top');
 // クリックした時の位置を取得
