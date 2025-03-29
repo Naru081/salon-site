a03738a7188e620f7f06b3f7e0259533fe6d979e
生川晴己 <oikawaharuonore@NaruMac.local>
2025-03-30 03:42:23 +0900

    javaを変更
    全てヘッダーのボタンにスクロール機能を実装しました。

--- java/index.java
+++ java/index.java
@@ -1,5 +1,6 @@
-// ヘッダーのボタン
-const topButton = document.querySelector('#header a.nav-link top');
+// ヘッダーバーのスクロール
+// TOPのボタン
+const topButton = document.querySelector('#header a.nav-link.top');
 // クリックした時の位置を取得
 topButton.addEventListener('click', () => {
     window.scrollTo({
@@ -7,12 +8,64 @@ topButton.addEventListener('click', () => {
         behavior: 'smooth' 
     });
 });
+// ABOUTのボタン
+const aboutButton = document.querySelector('#header a.nav-link.about');
+// クリックした時に#aboutまでスクロール
+aboutButton.addEventListener('click', (event) => {
+    event.preventDefault(); // デフォルトのリンク動作を無効化
+    const aboutSection = document.querySelector('#about'); // #aboutセクションを取得
+    const offsetTop = aboutSection.offsetTop; // #aboutの位置を取得
+
+    window.scrollTo({
+        top: offsetTop, // #aboutの位置までスクロール
+        behavior: 'smooth'
+    });
+});
+// MENUのボタン
+const menuButton = document.querySelector('#header a.nav-link.menu');
+// クリックした時に#aboutまでスクロール
+menuButton.addEventListener('click', (event) => {
+    event.preventDefault(); // デフォルトのリンク動作を無効化
+    const aboutSection = document.querySelector('#menu'); // #menuセクションを取得
+    const offsetTop = aboutSection.offsetTop; // #menuの位置を取得
+
+    window.scrollTo({
+        top: offsetTop, // #menuの位置までスクロール
+        behavior: 'smooth'
+    });
+});
+// ACCESSボタン
+const accessButton = document.querySelector('#header a.nav-link.access');
+// クリックした時に#accessまでスクロール
+accessButton.addEventListener('click', (event) => {
+    event.preventDefault(); // デフォルトのリンク動作を無効化
+    const aboutSection = document.querySelector('#access'); // #accessセクションを取得
+    const offsetTop = aboutSection.offsetTop; // #accessの位置を取得
+
+    window.scrollTo({
+        top: offsetTop, // #accessの位置までスクロール
+        behavior: 'smooth'
+    });
+});
+// NEWSボタン
+const newsButton = document.querySelector('#header a.nav-link.news');
+// クリックした時に#accessまでスクロール
+newsButton.addEventListener('click', (event) => {
+    event.preventDefault(); // デフォルトのリンク動作を無効化
+    const aboutSection = document.querySelector('#news'); // #accessセクションを取得
+    const offsetTop = aboutSection.offsetTop; // #accessの位置を取得
+
+    window.scrollTo({
+        top: offsetTop, // #accessの位置までスクロール
+        behavior: 'smooth'
+    });
+});
 
 
 // フッターのボタン
-const topButton = document.querySelector('#footer div.top');
+const footerTopButton = document.querySelector('#footer button');
 // クリックした時の位置を取得
-topButton.addEventListener('click', () => {
+footerTopButton.addEventListener('click', () => {
     window.scrollTo({
         top: 0, 
         behavior: 'smooth' 
