b0376152f0f4cb52fccd9eb7ddee4c48760ce6f3
生川晴己 <oikawaharuonore@NaruMac.local>
2025-04-27 13:50:10 +0900

    dbの接続先を変更
    ローカルからロリポップのデータベースに接続するように変更

--- php/db_connect.php
+++ php/db_connect.php
@@ -1,14 +1,13 @@
 <?php
 function getDatabaseConnection() {
-    $username = "root"; // XAMPPのデフォルトユーザー
-    $password = "";     // パスワード未設定（必要なら入力）
-    $dbname = "mydb";   // データベース名を指定
-
-    // ソケットパスを指定
-    $socket = "/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock";
+    $servername = "mysql321.phy.lolipop.lan";
+    $username = "LAA1661715";
+    $password = "Ub4CHy2";
+    $dbname = "LAA1661715-mydb";
 
     try {
-        $dsn = "mysql:unix_socket=$socket;dbname=$dbname;charset=utf8mb4";
+        // ソケットではなくホスト名経由で接続
+        $dsn = "mysql:host=$servername;dbname=$dbname;charset=utf8mb4";
         $pdo = new PDO($dsn, $username, $password);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
@@ -17,3 +16,4 @@ function getDatabaseConnection() {
         exit;
     }
 }
+?>
